f28d0a04edcefdb516ac509ef7eaf118d92225f8
Grisha <just.grishaayf@gmail.com>
2024-03-30 14:24:08 +0300

    Переписал через просто Item

--- Item.py
+++ Item.py
@@ -5,20 +5,11 @@ from dataclasses import dataclass, asdict
 class Item:
     name: str
     price: int
-
-
-@dataclass
-class ProviderItem(Item):
     provider_id: int
+    store_id: int = 0
 
     def __key(self):
         return (self.name, self.provider_id)
 
     def __hash__(self):  # делаем hashable чтобы класс можно было использовать в качестве ключа словаря
         return hash(self.__key())
-
-
-@dataclass
-class StoreItem:
-    item: Item
-    Store_id: int
